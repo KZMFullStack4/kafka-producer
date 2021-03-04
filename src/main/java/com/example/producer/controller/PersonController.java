c7c78c5fa20819d324277c1d19de7d6931e75a9d
Saeid Kazemi <saeidkazemi78java@gmail.com>
2021-03-04 15:41:43 +0330

    First commit

---
+++ src/main/java/com/example/producer/controller/PersonController.java
@@ -0,0 +1,13 @@
+package com.example.producer.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class PersonController {
+    public ResponseEntity<Void> createPerson()
+}
